904cd98fb2e419b657e11081e75278ea487e3488
me_user <1439996123@qq.com>
2019-04-24 10:06:29 +0800

    start

---
+++ src/main/java/com/sf/boot/FashionalApplication.java
@@ -0,0 +1,13 @@
+package com.sf.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionalApplication.class, args);
+	}
+
+}
